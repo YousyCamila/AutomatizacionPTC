0693a64a98ebec5ff59a7667ed2dffc75484ff06
YousyCamila <166034381+YousyCamila@users.noreply.github.com>
2025-05-20 12:12:53 -0500

    feat

--- LoginFacebook/src/main/java/co/com/AutoFacebook/userinterface/InicioFacebook.java
+++
@@ -1,8 +0,0 @@
-package co.com.AutoFacebook.userinterface;
-import net.serenitybdd.core.pages.PageObject;
-import net.thucydides.core.annotations.DefaultUrl;
-
-@DefaultUrl(value = "https://www.facebook.com/")
-
-public class InicioFacebook extends PageObject{
-}
