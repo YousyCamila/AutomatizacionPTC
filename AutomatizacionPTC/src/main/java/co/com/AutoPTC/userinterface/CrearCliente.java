8adb091f67f53a712b0df4ce5655a14c3f2d559c
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-06-03 15:19:56 -0500

    feat(Automatizacion): Refactorizar Interfaz

--- AutomatizacionPTC/src/main/java/co/com/AutoPTC/userinterface/CrearCliente.java
+++ AutomatizacionPTC/src/main/java/co/com/AutoPTC/userinterface/CrearCliente.java
@@ -24,19 +24,21 @@ public class CrearCliente {
     }
 
     public static final Target INPUT_NUMERO_DOCUMENTO = Target.the("Campo para el número de documento")
-            .located(By.xpath("//*[@id=\":r7e:\"]"));
+            .located(By.xpath("//*[@id=\":r7k:\"]"));
+
+
 
     public static final Target INPUT_NOMBRES = Target.the("Campo para los nombres del cliente")
-            .located(By.xpath("//*[@id=\":r7f:\"]"));
+            .located(By.xpath("//*[@id=\":r7l:\"]"));
 
     public static final Target INPUT_APELLIDOS = Target.the("Campo para los apellidos del cliente")
-            .located(By.xpath("//*[@id=\":r7g:\"]"));
+            .located(By.xpath("//*[@id=\":r7m:\"]"));
 
     public static final Target INPUT_CORREO_ELECTRONICO = Target.the("Campo para el correo electrónico")
-            .located(By.xpath("//*[@id=\":r7h:\"]"));
+            .located(By.xpath("//*[@id=\":r7n:\"]"));
 
     public static final Target INPUT_FECHA_NACIMIENTO = Target.the("Campo para la fecha de nacimiento")
-            .located(By.xpath("//*[@id=\":r7i:\"]"));
+            .located(By.xpath("//*[@id=\":r7o:\"]"));
 
     public static final Target BTN_GUARDAR_CLIENTE = Target.the("Botón para guardar el nuevo cliente")
             .located(By.xpath("//*[@id=\"root\"]/div/div[2]/div/div/form/div[2]/button[2]"));
