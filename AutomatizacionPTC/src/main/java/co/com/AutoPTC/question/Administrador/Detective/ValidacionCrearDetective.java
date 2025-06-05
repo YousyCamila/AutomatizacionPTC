9a3793f547bb638763cc1a3e69904e1b40cccc47
Juan Sebastian Odoñez Sierra <juanseordo2206@outlook.com>
2025-06-04 22:24:56 -0500

    feat: Construccion de validacion mas rubusta para la creacion de detective en un 100%

--- AutomatizacionPTC/src/main/java/co/com/AutoPTC/question/Administrador/Detective/ValidacionCrearDetective.java
+++ AutomatizacionPTC/src/main/java/co/com/AutoPTC/question/Administrador/Detective/ValidacionCrearDetective.java
@@ -1,36 +1,96 @@
 package co.com.AutoPTC.question.Administrador.Detective;
 
 import co.com.AutoPTC.models.Administrador.Detective.Detective;
+import net.serenitybdd.core.pages.WebElementFacade;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Question;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.waits.WaitUntil;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 import static co.com.AutoPTC.userinterface.Administrador.Detective.CreacionDetective.*;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isEnabled;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
 
 public class ValidacionCrearDetective implements Question<Boolean> {
 
     public static ValidacionCrearDetective esExitosa() {
         return new ValidacionCrearDetective();
     }
-
     @Override
     public Boolean answeredBy(Actor actor) {
-        boolean mensajeVisible = MENSAJE_CONFIRMACION_EXITO.resolveFor(actor).isVisible();
+        System.out.println("DEBUG: Iniciando la validación de creación de detective exitosa.");
 
-        
+        boolean mensajeVisible = MENSAJE_CONFIRMACION_EXITO.resolveFor(actor).isVisible();
+        System.out.println("DEBUG: ¿Mensaje de confirmación de éxito visible? " + mensajeVisible);
 
         Detective detective = actor.recall("datosDetective");
+        if (detective != null) {
+            System.out.println("DEBUG: Datos del detective recordados:");
+            System.out.println("DEBUG:   Nombre: " + detective.getNombres());
+            System.out.println("DEBUG:   Apellido: " + detective.getApellidos());
+            System.out.println("DEBUG:   Documento: " + detective.getNumeroDocumento());
+        } else {
+            System.err.println("ERROR: No se pudieron recordar los datos del detective. 'datosDetective' es nulo.");
+            return false;
+        }
 
-        // ✅ Aquí usamos withArguments directamente sobre el Target
-        boolean existePorCedula = FILA_POR_CEDULA
-                .resolveAllFor(actor)
-                .stream()
-                .anyMatch(elemento -> elemento.getText().contains(detective.getNumeroDocumento()));
+        actor.attemptsTo(
+                WaitUntil.the(BOTON_OK, isVisible()).forNoMoreThan(8).seconds(),
+                WaitUntil.the(BOTON_OK, isEnabled()).forNoMoreThan(8).seconds(),
+                Click.on(BOTON_OK)
+        );
+        System.out.println("DEBUG: Se hizo clic en el botón OK de la confirmación.");
 
-        boolean existePorNombreApellido = FILA_POR_NOMBRE_APELLIDO
-                .of(detective.getNombres(), detective.getApellidos())
-                .resolveAllFor(actor)
-                .size() > 0;
+        String nombreParaBusqueda = detective.getNombres().toUpperCase();
+        actor.attemptsTo(
+                WaitUntil.the(CAMPO_BUSQUEDA, isEnabled()).forNoMoreThan(8).seconds(),
+                Enter.theValue(nombreParaBusqueda).into(CAMPO_BUSQUEDA)
+        );
+        System.out.println("DEBUG: Se esperó al campo de búsqueda y se ingresó el nombre '" + nombreParaBusqueda + "'.");
 
-        return mensajeVisible && (existePorCedula || existePorNombreApellido);
+        actor.attemptsTo(
+                Click.on(BUSCAR)
+        );
+        System.out.println("DEBUG: Se hizo clic en el botón Buscar.");
+
+        String numeroDocumentoUpper = detective.getNumeroDocumento().toUpperCase();
+        String nombresUpper = detective.getNombres().toUpperCase();
+        String apellidosUpper = detective.getApellidos().toUpperCase();
+
+
+        List<WebElementFacade> elementosPorCedula = FILA_POR_CEDULA
+                .of(numeroDocumentoUpper)
+                .resolveAllFor(actor);
+
+        boolean existePorCedula = elementosPorCedula.stream()
+                .anyMatch(elemento -> {
+                    String textoElemento = elemento.getText();
+                    System.out.println("DEBUG: Texto de elemento evaluado para Cédula: '" + textoElemento + "'");
+                    return textoElemento.contains(numeroDocumentoUpper);
+                });
+        System.out.println("DEBUG: ¿Detective encontrado por Cédula (" + numeroDocumentoUpper + ")? " + existePorCedula);
+        System.out.println("DEBUG: Cantidad de elementos encontrados por Cédula: " + elementosPorCedula.size());
+
+        List<WebElementFacade> elementosPorNombreApellido = FILA_POR_NOMBRE_APELLIDO_EN_UNA_CELDA
+                .of(nombresUpper, apellidosUpper)
+                .resolveAllFor(actor);
+
+        boolean existePorNombreApellido = elementosPorNombreApellido.size() > 0;
+        System.out.println("DEBUG: ¿Detective encontrado por Nombre y Apellido unificados ('" + nombresUpper + " " + apellidosUpper + "')? " + existePorNombreApellido);
+        System.out.println("DEBUG: Cantidad de elementos encontrados por Nombre y Apellido: " + elementosPorNombreApellido.size());
+        if (!elementosPorNombreApellido.isEmpty()) {
+            for (WebElement elemento : elementosPorNombreApellido) {
+                System.out.println("DEBUG: Texto de elemento evaluado para Nombre/Apellido: '" + elemento.getText() + "'");
+            }
+        }
+
+        boolean resultadoFinal = mensajeVisible && (existePorCedula || existePorNombreApellido);
+        System.out.println("DEBUG: Resultado final de la validación de creación de detective: " + resultadoFinal);
+
+        return resultadoFinal;
     }
 }
--- AutomatizacionPTC/src/main/java/co/com/AutoPTC/userinterface/Administrador/Detective/CreacionDetective.java
+++ AutomatizacionPTC/src/main/java/co/com/AutoPTC/userinterface/Administrador/Detective/CreacionDetective.java
@@ -13,43 +13,37 @@ public class CreacionDetective {
             .located(By.xpath("//*[@id=\"root\"]/div/div[2]/div[1]/div[1]/button[1]"));
     public static final Target ABRIR_DROPDOWN_TIPO_DOCUMENTO = Target.the("Dropdown para tipo de documento")
             .located(By.xpath("//label[contains(text(), 'Tipo de Documento')]/following-sibling::div"));
-
     public static Target OPCION_TIPO_DOCUMENTO(String tipoDocumento) {
         return Target.the("Opción " + tipoDocumento)
                 .located(By.xpath("//ul[@role='listbox']//li[normalize-space(.)='" + tipoDocumento + "']"));
     }
-
     public static final Target INPUT_NUMERO_DOCUMENTO =
             Target.the("Campo número de documento")
                     .located(By.xpath("//label[text()='Número de Documento']/following-sibling::div//input"));
-
-    // CAMBIOS CLAVE AQUÍ: Usando el texto de la etiqueta para los TextField
     public static final Target INPUT_NOMBRES = Target.the("Campo nombres")
             .located(By.xpath("//label[normalize-space(text())='Nombres']/following-sibling::div//input"));
-
     public static final Target INPUT_APELLIDOS = Target.the("Campo apellidos")
             .located(By.xpath("//label[normalize-space(text())='Apellidos']/following-sibling::div//input"));
-
     public static final Target INPUT_CORREO_ELECTRONICO = Target.the("Campo correo electrónico")
             .located(By.xpath("//label[normalize-space(text())='Correo Electrónico']/following-sibling::div//input"));
-
-    // Este ya estaba bien, ya que el input de fecha tiene un type="date"
     public static final Target INPUT_FECHA_NACIMIENTO = Target.the("Campo fecha de nacimiento")
             .located(By.xpath("//label[normalize-space(text())='Fecha de Nacimiento']/following-sibling::div//input"));
-
     public static Target checkboxEspecialidad(String especialidades) {
         return Target.the("Checkbox de especialidad " + especialidades)
                 .located(By.xpath("//label[contains(@class, 'MuiFormControlLabel-root') and .//span[normalize-space(text())='" + especialidades + "']]"));
     }
     public static final Target BOTON_GUARDAR_DETECTIVE = Target.the("Botón guardar detective")
             .located(By.xpath("//button[normalize-space(text())='Guardar' or contains(., 'Guardar')]"));
-
     public static final Target MENSAJE_CONFIRMACION_EXITO = Target.the("Mensaje de confirmación de éxito")
             .located(By.id("swal2-html-container"));
-
-    public static final Target FILA_POR_CEDULA = Target.the("Fila por cédula")
-            .locatedBy("//table[@id='tablaDetectives']//tbody/tr[td[contains(text(),'{0}')]]");
-
-    public static final Target FILA_POR_NOMBRE_APELLIDO = Target.the("Fila por nombre y apellido")
-            .locatedBy("//table[@id='tablaDetectives']//tbody/tr[td[contains(text(),'{0}')] and td[contains(text(),'{1}')]]");
+    public static final Target BOTON_OK = Target.the("botón OK de alerta")
+            .located(By.xpath("//div[contains(@class, 'swal2-actions')]/button[contains(@class, 'swal2-confirm') and text()='OK']"));
+    public static final Target BUSCAR = Target.the("botón para buscar")
+            .located(By.xpath("//*[@id=\"root\"]/div/div[2]/div[1]/div[2]/button"));
+    public static final Target CAMPO_BUSQUEDA = Target.the("campo de búsqueda de detective")
+            .located(By.xpath("//*[@id=\"root\"]/div/div[2]/div[1]/div[2]/div/input"));
+    public static final Target FILA_POR_CEDULA = Target.the("fila de la tabla por cédula '{0}'")
+            .locatedBy("//tbody[contains(@class,'MuiTableBody-root')]//tr[./td[1][contains(text(),'{0}')]]");
+    public static final Target FILA_POR_NOMBRE_APELLIDO_EN_UNA_CELDA = Target.the("fila de la tabla por nombre '{0}' y apellido '{1}'")
+            .locatedBy("//tbody[contains(@class,'MuiTableBody-root')]//tr[./td[2][contains(text(),'{0}') and contains(text(),'{1}')]]");
 }
